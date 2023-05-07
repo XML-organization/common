15f00ac411f2040f2489f48fd77b719942b36b01
Dejan Gloginjic <deki555@hotmail.com>
2023-05-07 18:16:18 +0200

    added create user event

--- saga/create_user/create_user_event.go
+++ saga/create_user/create_user_event.go
@@ -1,7 +1,5 @@
 package create_user
 
-import "github.com/google/uuid"
-
 type Role int
 
 const (
@@ -11,16 +9,15 @@ const (
 )
 
 type User struct {
-	ID       uuid.UUID `json:"id"`
-	Email    string    `json:"email"`
-	Password string    `json:"password"`
-	Role     Role      `json:"role"`
-	Name     string    `json:"nameuuid`
-	Surname  string    `json:"surname"`
-	Country  string    `json:"country" gorm:"not null;type:string"`
-	City     string    `json:"city" gorm:"not null;type:string"`
-	Street   string    `json:"street" gorm:"not null;type:string"`
-	Number   string    `json:"number" gorm:"not null;type:string"`
+	Email    string `json:"email"`
+	Password string `json:"password"`
+	Role     Role   `json:"role"`
+	Name     string `json:"nameuuid`
+	Surname  string `json:"surname"`
+	Country  string `json:"country" gorm:"not null;type:string"`
+	City     string `json:"city" gorm:"not null;type:string"`
+	Street   string `json:"street" gorm:"not null;type:string"`
+	Number   string `json:"number" gorm:"not null;type:string"`
 }
 
 type CreateUserCommandType int8
@@ -43,6 +40,7 @@ const (
 	UserSaved CreateUserReplyType = iota
 	UserNotSaved
 	UserCredentialsDeleted
+	SuccessfulyFinished
 	UnknownReply
 )
 
