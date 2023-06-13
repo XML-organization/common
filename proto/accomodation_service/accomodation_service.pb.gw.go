ea7c18959943a16bbf7453a074ba9dc537e66cdf
Dejan Gloginjic <deki555@hotmail.com>
2023-06-13 18:40:09 +0200

    saving changes

--- proto/accomodation_service/accomodation_service.pb.gw.go
+++ proto/accomodation_service/accomodation_service.pb.gw.go
@@ -409,6 +409,40 @@ func local_request_AccommodationService_GradeHost_0(ctx context.Context, marshal
 
 }
 
+func request_AccommodationService_GetAccommodationRecommendations_0(ctx context.Context, marshaler runtime.Marshaler, client AccommodationServiceClient, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetAccommodationRecommendationsRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := client.GetAccommodationRecommendations(ctx, &protoReq, grpc.Header(&metadata.HeaderMD), grpc.Trailer(&metadata.TrailerMD))
+	return msg, metadata, err
+
+}
+
+func local_request_AccommodationService_GetAccommodationRecommendations_0(ctx context.Context, marshaler runtime.Marshaler, server AccommodationServiceServer, req *http.Request, pathParams map[string]string) (proto.Message, runtime.ServerMetadata, error) {
+	var protoReq GetAccommodationRecommendationsRequest
+	var metadata runtime.ServerMetadata
+
+	newReader, berr := utilities.IOReaderFactory(req.Body)
+	if berr != nil {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", berr)
+	}
+	if err := marshaler.NewDecoder(newReader()).Decode(&protoReq); err != nil && err != io.EOF {
+		return nil, metadata, status.Errorf(codes.InvalidArgument, "%v", err)
+	}
+
+	msg, err := server.GetAccommodationRecommendations(ctx, &protoReq)
+	return msg, metadata, err
+
+}
+
 // RegisterAccommodationServiceHandlerServer registers the http handlers for service AccommodationService to "mux".
 // UnaryRPC     :call AccommodationServiceServer directly.
 // StreamingRPC :currently unsupported pending https://github.com/grpc/grpc-go/issues/906.
@@ -637,6 +671,31 @@ func RegisterAccommodationServiceHandlerServer(ctx context.Context, mux *runtime
 		}
 
 		forward_AccommodationService_GradeHost_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
+	mux.Handle("POST", pattern_AccommodationService_GetAccommodationRecommendations_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		var stream runtime.ServerTransportStream
+		ctx = grpc.NewContextWithServerTransportStream(ctx, &stream)
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateIncomingContext(ctx, mux, req, "/accomodation.AccommodationService/GetAccommodationRecommendations", runtime.WithHTTPPathPattern("/accomodation/getRecommendations"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := local_request_AccommodationService_GetAccommodationRecommendations_0(annotatedContext, inboundMarshaler, server, req, pathParams)
+		md.HeaderMD, md.TrailerMD = metadata.Join(md.HeaderMD, stream.Header()), metadata.Join(md.TrailerMD, stream.Trailer())
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AccommodationService_GetAccommodationRecommendations_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
 
 	})
 
@@ -879,6 +938,28 @@ func RegisterAccommodationServiceHandlerClient(ctx context.Context, mux *runtime
 
 	})
 
+	mux.Handle("POST", pattern_AccommodationService_GetAccommodationRecommendations_0, func(w http.ResponseWriter, req *http.Request, pathParams map[string]string) {
+		ctx, cancel := context.WithCancel(req.Context())
+		defer cancel()
+		inboundMarshaler, outboundMarshaler := runtime.MarshalerForRequest(mux, req)
+		var err error
+		var annotatedContext context.Context
+		annotatedContext, err = runtime.AnnotateContext(ctx, mux, req, "/accomodation.AccommodationService/GetAccommodationRecommendations", runtime.WithHTTPPathPattern("/accomodation/getRecommendations"))
+		if err != nil {
+			runtime.HTTPError(ctx, mux, outboundMarshaler, w, req, err)
+			return
+		}
+		resp, md, err := request_AccommodationService_GetAccommodationRecommendations_0(annotatedContext, inboundMarshaler, client, req, pathParams)
+		annotatedContext = runtime.NewServerMetadataContext(annotatedContext, md)
+		if err != nil {
+			runtime.HTTPError(annotatedContext, mux, outboundMarshaler, w, req, err)
+			return
+		}
+
+		forward_AccommodationService_GetAccommodationRecommendations_0(annotatedContext, mux, outboundMarshaler, w, req, resp, mux.GetForwardResponseOptions()...)
+
+	})
+
 	return nil
 }
 
@@ -900,6 +981,8 @@ var (
 	pattern_AccommodationService_CheckIfGuestHasReservationInPast_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"accomodation", "CheckIfGuestHasReservationInPast"}, ""))
 
 	pattern_AccommodationService_GradeHost_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"accomodation", "gradeHost"}, ""))
+
+	pattern_AccommodationService_GetAccommodationRecommendations_0 = runtime.MustPattern(runtime.NewPattern(1, []int{2, 0, 2, 1}, []string{"accomodation", "getRecommendations"}, ""))
 )
 
 var (
@@ -920,4 +1003,6 @@ var (
 	forward_AccommodationService_CheckIfGuestHasReservationInPast_0 = runtime.ForwardResponseMessage
 
 	forward_AccommodationService_GradeHost_0 = runtime.ForwardResponseMessage
+
+	forward_AccommodationService_GetAccommodationRecommendations_0 = runtime.ForwardResponseMessage
 )
